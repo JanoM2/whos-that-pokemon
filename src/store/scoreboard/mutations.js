390e442e6f3df959bbeeecfb28e9e9dc9e673f95
Jano <martinojano0@gmail.com>
2023-11-27 15:43:27 -0300

    delete jest

--- src/store/scoreboard/mutations.js
+++ src/store/scoreboard/mutations.js
@@ -1,5 +1,5 @@
 export const setNames = (state, arrNames) => {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 9; i++) {
     arrNames.sort((a, b) => b.counter - a.counter);
     state.names.push(arrNames);
   }
